e422a5466430a2d8844c2b739e1561127f5b378f
janus.zhou <janus.zhou@wisebanyan.com>
2016-10-27 18:26:48 -0400

    Update script, using vagrant reload --provision and add config you can edit based on your setting

--- addsite.sh
+++ addsite.sh
@@ -4,6 +4,11 @@ echo "Simple way to add nginx sites to Homestead"
 echo "Use: addsite <project name> <folder path> <vagrant ip> <vagrant path>"
 echo
 
+vagrantIp="192.168.10.10" # change it if you set different vagrant box ip address
+baseVagrantPath="/home/vagrant/Code/" # mapped vagrant box folder path
+baseProjectPath="/Users/zhou/public_html/" # local folder path
+baseHomesteadFolder="/Users/zhou/.homestead" # homestead yaml file path
+homesteadPath="/Users/zhou/public_html/Homestead" # homestead vagrant file path, where you usually run vagrant up & halt
 
 function lowercase {
 	lowercased=`echo $1 | tr '[A-Z]' '[a-z]'`
@@ -23,58 +28,15 @@ if [ -z "$2" ]
 	then
 		echo
 		echo "Path to site's public directory on host machine. "
-		echo "Path will be prefixed with your home directory. "
+		echo "Path will be prefixed with your config. "
 		echo -n "Folder to use: "
 		read answer
 	else
 		answer="$2"
 fi
 
-vSiteFolder="/home/vagrant/$answer"
-siteFolder="$(pwd)/$answer"
-
-if [ -z "$3" ]
-	then
-		echo
-		echo "Homestead's IP address "
-		echo -n "(leave blank for 192.168.10.10): "
-		read answer
-
-		if [ -z "$answer" ]
-			then
-				answer="192.168.10.10"
-		fi
-
-	else
-		answer="$3"
-fi
-
-vagrantIp="$answer"
-
-if [ -z "$4" ]
-	then
-		echo "Homestead's folder path "
-		echo -n "(leave blank for $(pwd)/Code/Homestead): "
-		read answer
-
-		if [ -z "$answer" ]
-			then
-				answer="$(pwd)/Code/Homestead"
-		fi
-
-	else
-		answer="$4"
-fi
-
-vagrantPath="$answer"
-
-# create folder if project doesn't exist
-echo
-echo "Creating folder $siteFolder ..."
-mkdir -p "$siteFolder"
-
-echo "Connecting to Homestead ..."
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no vagrant@127.0.0.1 -p 2222 "sudo bash /vagrant/scripts/serve.sh $siteName $vSiteFolder"
+vSiteFolder="$baseVagrantPath$answer/"
+siteFolder="$baseProjectPath$answer/"
 
 echo "Editing hosts file (You might be asked to provide your user's password for host machine)..."
 # edit hosts file
@@ -82,11 +44,12 @@ echo -e "\n$vagrantIp $siteName" | sudo tee -a /etc/hosts
 
 echo "Editing Homestead.yaml file ..."
 # edit Homestead.yaml
-if [ -e "$(pwd)/.homestead/Homestead.yaml" ]
+if [ -e "$baseHomesteadFolder/Homestead.yaml" ]
 	then
-		file="$(pwd)/.homestead/Homestead.yaml"
+		file="$baseHomesteadFolder/Homestead.yaml"
 	else
-		file="$vagrantPath/Homestead.yaml"
+		echo "\n Homestead.yaml file path error"
+		exit 1
 fi
 
 siteFolderLength=${#siteFolder}
@@ -103,15 +66,16 @@ if [ ${siteFolder:(-7)}  == "/public" ]
 		vSiteFolderShort=${vSiteFolder:0:$vEndLength}
 fi
 
-awk -v from="folders:.*$" -v to="folders:\n    - map: $sFolder\n      to: $vSiteFolderShort" '{gsub(from,to,$0); print $0}' $file > ${file}.new
-awk -v from="sites:.*$" -v to="sites:\n    - map: $siteName\n      to: $vSiteFolder" '{gsub(from,to,$0); print $0}' ${file}.new > ${file}.new2
+cd "$baseHomesteadFolder"
+
+awk -v from="sites:.*$" -v to="sites:\n    - map: $siteName\n      to: $vSiteFolder" '{gsub(from,to,$0); print $0}' ${file} > ${file}.new
 
 echo "Removing temp files ..."
-rm -f ${file}.new
-mv ${file}.new2 $file
+mv ${file}.new $file
 
+cd $homesteadPath
 echo "Restarting your Homestead VM..."
-homestead halt && homestead up
+vagrant reload --provision
 
 echo "All done! Thank you!"
 echo
